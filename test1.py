84cdac4000a27be890f9c4251787bfecdd0c4f81
GeoSolve <GeoSolve@users.noreply.github.com>
2015-09-13 13:15:32 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,21 +1,14 @@
 w = 'Hello Python World!'
-
 print w
+print '\n'
 
-
+hw = 'Hello World'
+C_hw = hw.upper()
+fid = open('test.dat','w')
+for i in range(len(C_hw)):
+    print C_hw[i]
+    fid.write(C_hw[i])
+    fid.write('\n')
+fid.close()
 
 print 'Finished writing Hellow World to a file!'
-
-a=1
-b=2
-c=2.0
-
-k=a/b
-p=a/c
-
-print 'If:'
-print '  a=',a,',b=',b,', c=',c
-print 'k=a/b','p=a/c'
-print 'Then:'
-print 'k=',k
-print 'p=',p
